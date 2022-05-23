0e5a2cd3baaa2b1624c5371ed01a2fd89e486682
Mohamed Eid Saad <m.eid.saad403@gmail.com>
2022-05-23 16:18:14 +0200

    2 days left

--- routes/web.php
+++ routes/web.php
@@ -104,25 +104,7 @@ Route::post('loginhome' , [PatientController::class, 'postLogin'])->name('patien
 Auth::routes();
 
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('user')->name('user.')->group(function(){
-  
-    Route::middleware(['guest:web','PreventBackHistory'])->group(function(){
-          Route::view('/login','dashboard.user.login')->name('login');
-          Route::view('/register','dashboard.user.register')->name('register');
-          Route::post('/create',[UserController::class,'create'])->name('create');
-          Route::post('/check',[UserController::class,'check'])->name('check');
-    });
-
-    Route::middleware(['auth:web','PreventBackHistory'])->group(function(){
-          Route::view('/home','dashboard.user.home')->name('home');
-          Route::post('/logout',[UserController::class,'logout'])->name('logout');
-          Route::get('/add-new',[UserController::class,'add'])->name('add');
-          
-    });
-
-});
 
 Route::prefix('admin')->group(function(){
 
