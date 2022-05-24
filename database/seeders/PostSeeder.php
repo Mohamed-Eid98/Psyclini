efdbf762c884696eaed6c3a8022c38e6d5c69c62
Mohamed Eid Saad <m.eid.saad403@gmail.com>
2022-05-24 04:06:25 +0200

    final

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -15,72 +15,66 @@ class PostSeeder extends Seeder
     public function run()
     {
         \App\Models\Post::create([
-            'body'=>'This is first Post',
+            'body'=>' Do not give up',
             'speciality'=>'life coach',
-             'image' => '1649750261.png',
-             'patient_id'=>'1',
+            'image' => '1649750261.png',
+            'patient_id'=>'1',
 
+            ]);
+        \App\Models\Post::create([
+            'body'=>'i finally got better and i wanted to share that with all of you',
+            'speciality'=>'life coach',
+            'image' => '1649750261.png',
+            'patient_id'=>'3',
+    
             ]);
         \App\Models\Post::create([
             'body'=>'i need a psychatry',
             'speciality'=>'Child and Adolescence Disorders',
-             'image' => '1649750261.png',
-             'patient_id'=>'2',
+            'image' => '1649750261.png',
+            'patient_id'=>'2',
 
             ]);
         \App\Models\Post::create([
-            'body'=>'This is third Post',
+            'body'=>'Is the therapy really working ?',
             'speciality'=>'General Psychiatry',
-             'image' => 'banner.png',
-             'patient_id'=>'3',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'This is first Post',
-            'speciality'=>'Geriatric Psychiatry',
-             'image' => 'banner.png',
-             'patient_id'=>'3',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'This is first Post',
-            'speciality'=>'Psychiatry of Intellectual Disability (PID)',
-             'image' => 'banner.png',
-             'patient_id'=>'2',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'This is first Post',
-            'speciality'=>'Marital and Family Relations',
-             'image' => 'banner.png',
-             'patient_id'=>'1',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'This is first Post',
-            'speciality'=>'life coach',
-             'image' => 'banner.png',
-             'patient_id'=>'2',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'This is first Post',
-            'speciality'=>'Addiction',
-             'image' => 'banner.png',
-             'patient_id'=>'2',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'This is first Post',
-            'speciality'=>'Forensic Psychiatry',
-             'image' => 'banner.png',
-             'patient_id'=>'3',
-
-            ]);
-        \App\Models\Post::create([
-            'body'=>'a new post',
-            'speciality'=>'Forensic Psychiatry',
+            'image' => 'banner.png',
             'patient_id'=>'3',
+
+            ]);
+        \App\Models\Post::create([
+            'body'=>'any one want to share thier story ?',
+            'speciality'=>'Geriatric Psychiatry',
+            'image' => 'doctors.png',
+            'patient_id'=>'1',
+
+            ]);
+        \App\Models\Post::create([
+            'body'=>'Are patients treated with care ?',
+            'speciality'=>'Psychiatry of Intellectual Disability (PID)',
+            'image' => 'banner.png',
+            'patient_id'=>'2',
+
+            ]);
+        \App\Models\Post::create([
+            'body'=>'I recommend those books for you (book1,book2,...)',
+            'speciality'=>'Marital and Family Relations',
+            'image' => 'test4.jpg',
+            'patient_id'=>'3',
+            ]);
+        \App\Models\Post::create([
+            'body'=>'It is easy to give up but you have to fight ',
+            'speciality'=>'Addiction',
+            'image' => 'doctors.png',
+            'patient_id'=>'2',
+
+            ]);
+        \App\Models\Post::create([
+            'body'=>'Many crimes are related to the mental health ',
+            'speciality'=>'Forensic Psychiatry',
+            'image' => 'banner.png',
+            'patient_id'=>'3',
+
             ]);
         }
 }
